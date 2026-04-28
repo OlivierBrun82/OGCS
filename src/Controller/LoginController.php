f745429bbfa762c46a7c8ed4b87fc2f193a2a658
Olivier Brun <contactpro@olivierbrun.eu>
2026-04-28 13:30:53 +0200

    added registration and login

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+final class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+        // si il y a une erreur de login, la récupère :
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // affiche le dernier utilisateur connecté :
+        $lastUserName = $authenticationUtils->getLastUsername();
+
+
+        return $this->render('login/index.html.twig', [
+            'controller_name' => 'LoginController',
+            'last_username' => $lastUserName,
+            'error' => $error,
+        ]);
+    }
+}
